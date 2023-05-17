edc9f4a16db1820412ed1dd7724afed318c6effd
Chayma-ab <abidallicha@gmail.com>
2023-05-17 10:42:47 +0100

    preparing the map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
+import {MapContainer, TileLayer, Marker, Popup  } from "react-leaflet";
+import L from"leaflet";
 import './App.css';
 
 function App() {
+  const position = [36.8065, 10.1815]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+      
     </div>
   );
 }
+
+let DefaultIcon = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.0.3/dist/images/marker-icon.png',
+
+})
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export default App;
